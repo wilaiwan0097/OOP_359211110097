bf1ec2d128102a27fdc5fa1636cb90af13ddaf84
wilaiwan0097 <chisuka301@gmail.com>
2017-11-21 15:20:10 +0700

    Testing Add Data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -3,5 +3,10 @@ package ooptest;
 public class HelloWorld {
     public static void main(String[] args) {
     System.out.println("Hello world,I'm TIK");
+  //  Add Data
+    System.out.println("wilaiwan");
+    System.out.println("Information System");
+    System.out.println("Faculty of Management Technology");
+    System.out.println("RMUTSV");
     }//main
 }//class
