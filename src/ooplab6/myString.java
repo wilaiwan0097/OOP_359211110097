153fdf0b2d5471a0c40bf4352de688deb4f34dcd
wilaiwan0097 <chisuka301@gmail.com>
2017-12-20 11:14:53 +0700

    myString

--- src/ooplab6/myString.java
+++ src/ooplab6/myString.java
@@ -2,6 +2,44 @@ package ooplab6;
 
 public class myString {
     public static void main(String[] args) {
+        String s;
+        s = new String();
+        s = "RMUTSV";
+        System.out.println(s);
+        s = "Welcome " + s;
+        System.out.println(s);
+        s = s.concat(" 2017");
+        System.out.println(s);
+        System.out.println(s.charAt(10));
+//        comparaion String
+        String s1 = "Hello";
+        String s2 = "hello";
+
+        if (s1==s2)
+            System.out.println("ข้อความนี้เหมือนกัน");
+        else System.out.println("ข้อความนี้ไม่เหมือนกัน");
+//        equal method
+        if (s1.equals (s2))
+            System.out.println("ข้อความนี้เหมือนกัน");
+        else System.out.println("ข้อความนี้ไม่เหมือนกัน");
+//        compareto method
+        if (s1.compareTo (s2)==0)
+            System.out.println("ข้อความนี้เหมือนกัน");
+        else  if (s1.compareTo (s2)<0)  //-
+            System.out.println("ข้อความทางซ้ายน้อยกว่า");
+        else  //+
+            System.out.println("ข้อความทางขวามากกว่า");
+        //trim method
+        String s3 = "  Hello  ";
+        System.out.println(s3.length());
+        System.out.println("*"+s3+"*");
+        System.out.println("*"+s3.trim()+"*");
+
+
+
+
+
+
 
 
 
---
+++ src/ooplab6/myStringBuffer.java
@@ -0,0 +1,4 @@
+package ooplab6;
+
+public class myStringBuffer {
+}
