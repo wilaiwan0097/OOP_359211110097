96426340cd60f279590fdf489511e57cd91fbf4f
wilaiwan0097 <chisuka301@gmail.com>
2017-12-20 10:51:35 +0700

    test Exercise

--- src/ooplab5/test2.java
+++ src/ooplab5/test2.java
@@ -1,7 +1,72 @@
 package ooplab5;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 public class test2 {
+    private static final int MAX = 20;
+    private static ArrayList even = new ArrayList();
+    private static ArrayList odd = new ArrayList();
     public static void main(String[] args) {
+        Integer[] num = new Integer[MAX];
+        num = inputData(num);
+        showData(num);
+        num = sortData(num);
+
+        validateEvenData(num);  //even
+        validateOddDate(num); //odd
+
+        System.out.println("Validate Data in array with Desending order: ");
+        showArrayList("even",even);
+        showArrayList("odd",odd);
+    }//main
+
+    private static Integer[] sortData(Integer[] num) {
+        Arrays.sort(num);
+        return num;
+    }
+
+    private static void showArrayList(String msg, ArrayList data) {
+        if (msg.equals("even"))
+            System.out.print("Even number: ");
+        else
+            System.out.print("Odd number: ");
+        for (int i=0;i<data.size();i++){
+            System.out.print(data.get(i)+" ");
+        }
+        System.out.println();
 
     }
-}
+
+    private static void validateOddDate(Integer[] num) {
+        for (int i=0;i<num.length;i++){
+            if (num[i]%2!=0)
+                odd.add(num[i]);
+        }
+    }
+
+    private static void validateEvenData(Integer[] num) {
+        for (int i=0;i<num.length;i++){
+            if (num[i]%2==0)
+                even.add(num[i]);
+        }
+    }
+
+    private static void showData(Integer[] num) {
+        System.out.println("Data in array: ");
+
+        for(int val: num)
+            System.out.print(val+" ");
+        System.out.println();
+    }
+
+    private static Integer[] inputData(Integer[] num) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        for(int i=0;i<num.length;i++){
+            System.out.print("num["+i+"]: ");
+            num[i] = scanner.nextInt();
+        }
+        return num;
+    }//inputdata
+
+}//class
---
+++ src/ooplab6/myString.java
@@ -0,0 +1,11 @@
+package ooplab6;
+
+public class myString {
+    public static void main(String[] args) {
+
+
+
+
+
+    }//main
+}//class
