cdd884085a334cb476a20258c84febe70540c866
wilaiwan0097 <chisuka301@gmail.com>
2018-02-07 01:18:05 +0700

    interface:Shape

---
+++ src/ooplab8/ShapeApp.java
@@ -0,0 +1,10 @@
+package ooplab8;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(20, 30);
+        System.out.println(rectangle.toString());
+        System.out.println("Object area: "+rectangle.getArea());
+
+    }//main
+}//class
