8fdde20866f985bcf954ffaeea53f7f664accf87
wilaiwan0097 <chisuka301@gmail.com>
2018-02-07 10:59:22 +0700

    Interitance:Edit

--- src/ooplab9/myPersonApp.java
+++ src/ooplab9/myPersonApp.java
@@ -4,8 +4,11 @@ public class myPersonApp {
     public static void main(String[] args) {
         Person person = new Person("1800400231493","Wilaiwan",
                 new Address("100 M.8","Nakhon Si Tammarat","80150"),
-                new Job("Student","1000"));
+                new Job("Student","10000"));
         System.out.println(person.toString());
+
+        person.getJob().setSalary(20000);
+        System.out.println("Name: "+person.getName()+" Salary: "+ person.getJob().getSalary());
 
 
 
